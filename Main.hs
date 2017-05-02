1c829060b7c36d80486096a87cfb63378766e650
petermcneil <petermcneil@users.noreply.github.com>
2017-05-02 23:02:41 +0100

    Implemented persistence

--- Main.hs
+++ Main.hs
@@ -1,9 +1,11 @@
+
 {-#LANGUAGE OverloadedStrings, EmptyDataDecls, FlexibleContexts, GADTs
    , GeneralizedNewtypeDeriving, MultiParamTypeClasses, QuasiQuotes, TemplateHaskell
    , TypeFamilies #-}
 
 import Application () -- for YesodDispatch instance
 import Foundation
+
 import Yesod.Core
 import Database.Persist.Sqlite
 import Control.Monad.Trans.Resource (runResourceT)
@@ -13,15 +15,6 @@ openConnectionCount :: Int
 openConnectionCount = 10
 
 main :: IO ()
-main = {-runStderrLoggingT $ withSqlitePool "db/haskCalc.db" openConnectionCount $ \pool -> liftIO $ do
-    runResourceT $ flip runSqlPool pool $ do
-        runMigration migrate
--}
-    warp 3000 App
-
-
-  {-  pool <- runStdoutLoggingT $ createSqlitePool "db/haskCalc" 10
-  runSqlPersistMPool (runMigration migrateAll) pool
-  
-  warp 3000 App
--}
+main = runStderrLoggingT $ withSqlitePool "db/haskCalc.db" openConnectionCount $ runResourceT $ flip runSqlPool pool $ do
+     runMigration migrateAll
+  warp 3000 $ App pool
